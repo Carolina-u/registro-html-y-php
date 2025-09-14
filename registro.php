400557e9ca5d748dc35c1e8f173a194047311b02
Carolina-u <ncarolinadiaz@ucundinamarca.edu.co>
2025-09-13 20:29:06 -0500

    Update2 registro.php

--- registro.php
+++ registro.php
@@ -16,11 +16,11 @@ if($conexionbasededatos->connect_error){
 //aqui se reciben los datos del registro en el coso de html
 $cedula=trim($_POST['cedula']);
 $nombre =trim($_POST['nombre']);
-$apellidos=trim($_POST['apellidos']);
-$correo=trim($_POST['correo']);
+$apellido=trim($_POST['apellidos']);
+$mail=trim($_POST['mail']);
 $contrasena=$_POST['contrasena'];
 $confirmocontrasena=$_POST['confirmocontrasena'];
-$aceptoterminos=isset($_POST['aceptoterminos']) ? 1:0;
+$acept_terminos_condiciones=isset($_POST['acept_terminos_condiciones']) ? 1:0;
 
 
 //mas validaciones de contraseña
@@ -29,35 +29,54 @@ if($contrasena !==$confirmocontrasena){
   die("Las contraseñas no coinciden.");
   
 }
-if(!$aceptoterminos){
+if(!$acept_terminos_condiciones){
   die("Debe aceptar los términos y condiciones.");
 }
+//saber si la cedula ya esta registrada
+$checkcedula=$conn->prepare("SELECT id FROM usuarios WHERE cedula=?");
+$checkcedula ->bind_param("s", $cedula);
+$checkcedula ->execute();
+$checkcedula ->store_result();
+
+if($checkcedula->num_rows > 0){
+  die("La cédula ya se encuentra registrada");
+}
+//saber si el mail ya esta registrado
+$checkmail=$conexionbasededatos->prepare("SELECT id FROM usuarios WHERE mail=?");
+$checkmail->bind_param("s", $mail);
+$checkmail->execute();
+$checkmail->store_result();
+
+if ($checkmail->num_rows>0){
+  die("El email ya está registrado");
+}
+
 
 //no se si ud ya tiene esa vaina encriptada pero aqui ta
-$contasenasegura=password_hash($contrasena, PASSWORD_BCRYPT);
+$contrasenasegura=password_hash($contrasena, PASSWORD_BCRYPT);
 
 
 //Ahora es la base de datos, aqui si tiene que meter la tabla de usuarios o no se como le puso
 //esto si es ia, lo tiene que meter, maria porque yo no lo tengo
-$sql="INSERT INTO usuarios (cedula, nombre, apellidos, correo, contrasena, aceptoterminos) VALUES (?, ?, ?, ?, ?)";
+$sql="INSERT INTO usuarios (cedula, nombre, apellidos, mail, contrasena, acept_terminos_condiciones) VALUES (?, ?, ?, ?, ?,?)";
 
 
 //ya esto no es ia
 $stmt= $conexionbasededatos->prepare($sql);
-$stmt->bind_param("ssssi", $cedula, $nombre, $apellidos, $correo, $contasenasegura, $aceptoterminos);
+$stmt->bind_param("ssssi", $cedula, $nombre, $apellido, $mail, $contrasena, $acept_terminos_condiciones);
 
 if ($stmt ->execute()){
   echo "Cuenta creada. Puedes <a href='login.html'>iniciar sesión</a>.";
 
 }else{
-  echo"Error al crear la cuenta: " . $stmt->error;
+  echo"Error: " . $stmt->error;
   
 }
 
 //cerra la conexion, tengo sueño, me tocó arreglar esta monda
 //aiuda
+$checkcedula->close();
+$checkmail->close();
 $stmt->close();
 $conexionbasededatos ->close();
 ?>
-File: registro.html
-
